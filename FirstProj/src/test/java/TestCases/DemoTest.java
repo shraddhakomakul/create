b8686a81ce5d8986628e9d52938b246e000ca31b
Utkarsh <utkarsh.srivastav@accenture.com>
2018-07-10 17:21:09 +0530

    new demo

---
+++ FirstProj/src/test/java/TestCases/DemoTest.java
@@ -0,0 +1,30 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class DemoTest {
+	WebDriver driver;	
+	
+	//demo
+	@Test
+	public void DemoTest() {
+		driver.get("https://mvnrepository.com");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.getProperty("webdriver.chrome.driver", "Resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		driver.close();
+	}
+
+}
