d1db17632b94869471a9e01ccec63aa9df56ccd8
Utkarsh <utkarsh.srivastav@accenture.com>
2018-07-10 16:09:07 +0530

    First Git Project

---
+++ FirstProj/src/test/java/TestCases/Demo1.java
@@ -0,0 +1,28 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Demo1 {
+	WebDriver driver;	
+	@Test
+	public void DemoTest() {
+		driver.get("http://www.google.com/");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.getProperty("webdriver.chrome.driver", "Resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		driver.close();
+	}
+
+}
